8302b357a0d3bd22d78d7b466130563c5bf0ba57
dylanmeehan <dylanmeehan@gmail.com>
2018-03-02 21:41:32 -0500

    Added plotting into testSTeerOffset.m

--- testSteerOffset.m
+++ testSteerOffset.m
@@ -6,10 +6,10 @@
 delta0 = 0;
 phi0 = 0;
 phi_dot0 = 0;
-x = 0; 
-y = 0;
+x0 = 0; 
+y0 = 0;
 psi0 = 0;
-v = 3;  %m/s
+v0 = 3;  %m/s
 
 
 %bike parameters: duplicated in runBicycleTest!
@@ -17,11 +17,12 @@ v = 3;  %m/s
   p.l = 1.02; %length of wheel base 
 
 %controller gains:
-%K= [11.1,2.4,-6.6]; %lqr
-%K= [23,16,-2]; %large grid search optimization
-%K= [71,21,-20]; %old gains from daniel (from nav sim)
-K= [71,10,-20]; %other old gains
+K1= [11.1,2.4,-6.6]; %lqr
+K2= [23,16,-2]; %large grid search optimization
+K3= [71,21,-20]; %old gains from daniel (from nav sim)
+K4= [71,10,-20]; %other old gains
 
+controller = [K1; K2; K3; K4];
 %nav instrcutions (as a vector of delta commands for each timestep
     %runBicycleTest calculates a lean offset to corresepond to a stable
     %turn at the desired Steer Angle
@@ -30,26 +31,99 @@ K= [71,10,-20]; %other old gains
 
 
 %delta_offset = makeStepOffset();
-t = linspace(0,5,400);
-delta_offset = (sin(t).*0.6)';
+t = linspace(0,2*pi,400);
+delta_offset = (sin(t).*pi./6)';
 
 numTimeSteps = length(delta_offset);
-phi_offset = v^2/p.l/p.g.*delta_offset; %steady state relation between phi & delta
+phi_offset = v0^2/p.l/p.g.*delta_offset; %steady state relation between phi & delta
 %^this line is duplicated in runBicycleTest!!
+
+angle = figure('Name','Steer tests with sine input');
+traj = figure('Name','Steer tests with sine input');figure(angle)
+ 
+for i = 1:4
+    K = controller(i,:);
     
-delta0 = delta_offset(1);
-phi0 = phi_offset(1);
+    [sucess,state] = runBicycleTest(x0,y0,v0,delta0,phi0,phi_dot0,psi0, ...
+    K,delta_offset,numTimeSteps, 0);
 
-[sucess,state] = runBicycleTest(x,y,v,delta0,phi0,phi_dot0,psi0, ...
-    K,delta_offset,numTimeSteps, 1);
+    %score: judge navigation ability based on how well the bike can 
+    %    achieve the desired steer angle
 
-phi = state(:,4);
-delta = state(:,6);
+    figure(angle)
+    times = state(:,1);
+    phi = state(:,4);
+    delta = state(:,6);
+    phidot = state(:,7);
+    deltadot = diff(delta)./diff(times);
+    psi = state(:,5);
+    x = state(:,2);
+    y = state(:,3);
 
-%score: judge navigation ability based on how well the bike can 
-%    achieve the desired steer angle
-nav_score = sum(sqrt((delta-delta_offset).^2))
-   
+    subplot(2,2,1)
+    plot(times,phi);
+    hold on
+    title('lean vs. time');
+    xlabel('time (s)');
+    ylabel('phi');
+    subplot(2,2,2)
+    hold on
+    plot(times,phidot);
+    title('lean rate vs. time');
+    xlabel('time (s)');
+    ylabel('phi-dot');
+    subplot(2,2,3)
+    hold on
+    plot(times,delta);
+    title('steer vs. time');
+    xlabel('time (s)');
+    ylabel('delta');
+    subplot(2,2,4)
+    hold on
+    plot(times(1:end-1),deltadot);
+    title('steer rate vs. time');
+    xlabel('time (s)');
+    ylabel('deltadot');
+    
+    figure(traj)
+    subplot(2,1,1)
+    hold on
+    plot(x,y);
+    axis equal
+    title('bicycle trajectory');
+    xlabel('x position');
+    ylabel('y position');
+    subplot(2,1,2)
+    hold on
+    psid = diff(psi)/diff(times);
+    plot(times(1:end-1),psid);
+    title('yaw dot vs. time');
+    xlabel('time (s)');
+    ylabel('yaw dot');
+    
+    
+    nav_score = sqrt(sum((delta-delta_offset).^2));
+    disp(K); disp(nav_score)
+    
+end
+
+figure(angle)
+subplot(2,2,1)
+plot(times,phi_offset);
+hold on
+subplot(2,2,3)
+plot(times,delta_offset);
+hold on
+subplot(2,2,1)
+legend ( "lqr [11.1, 2.4, -6.6]", "grid [23, 16, -2]", "Nav [71, 21, -20]",...
+    "other [71,10,-20]", "desired")
+shg
+
+figure(traj)
+legend ("lqr [11.1, 2.4, -6.6]", "grid [23, 16, -2]", "Nav [71, 21, -20]",...
+    "other [71,10,-20]")
+shg
+
 
 
 function delta_offset = makeStepOffset()
